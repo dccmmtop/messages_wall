6cbcde1d3469147cb0ad8bbe482197c2a5ace860
dccmmtop <dccmmtop@gmail.com>
2019-02-25 17:55:14 +0800

    注册

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
-  def set_validate_code
-    Raile.cache.write(@user.email,Settings.validate_code_time.minutes) do
-      [*'0'..'9',*'A'..'Z'].sample(6).join
-    end
-  end
 
-  def validate_code
-    Raile.cache.read(@user.email)
+  validates_length_of :nickname, in: 2..10, message: "昵称长度要小于10大于2"
+  validates_length_of :password, in: 6..16, message: "密码长度要小于16大于6"
+  validates_presence_of :email, message: "邮箱不能为空"
+  validates_presence_of :nickname, message: "昵称不能为空"
+  validates_uniqueness_of  :email,message: "邮箱已被注册"
+  validates_uniqueness_of  :nickname,message: "昵称已被占用"
+
+  def self.get_validate_code(email)
+    Rails.cache.read(email)
   end
 end
