1c9e512f1688b8dd1304b74a8c799caded358a7c
dccmmtop <dccmmtop@gmail.com>
2019-02-26 15:55:18 +0800

    获取某人的所有留言

--- app/api/messages_api.rb
+++ app/api/messages_api.rb
@@ -8,7 +8,7 @@ module Api
       requires :content, type: String, desc: "message content"
       requires :latitude, type: String, desc: "纬度"
       requires :longitude, type: String, desc: "经度"
-      requires :limit_user_account, type: Integer, desc: "观看人数限制"
+      requires :limit_user_accounts, type: Integer, desc: "观看人数限制"
       requires :limit_days, type: Integer, desc: "时间限制"
     end
     post "create" do 
@@ -40,7 +40,7 @@ module Api
       data[:message] = ""
       data[:sum] = @messages.count
       data[:result] = @messages.map do |m|
-        {id: m.id, latitude: m.latitude, longitude: m.longitude, content: m.content, limit_days: m.limit_days}
+        {id: m.id, latitude: m.latitude, longitude: m.longitude, content: m.content, limit_days: m.limit_days, owner: m.user.id}
       end
       return data
     end
@@ -60,16 +60,59 @@ module Api
 
     desc "删除留言"
     params do
+      requires :token, type: String, desc: "token"
       requires :id, type: Integer, desc: "留言id"
     end
-    get "delete_messages_by_id" do
-      m = Message.find_by_id(params[:id])
+    post "delete_messages_by_id" do
+      u = User.find_by_token(params[:token])
+      m = Message.find_by("user_id = ? and id = ?", u.id, params[:id])
       if m
         m.update(is_delete: true)
         return {status: 0,message: "删除成功"}
       else
         return {status: 202, message: '没有找到'}
       end
+    end
+
+    desc "更新留言"
+    params do
+      requires :token, type: String, desc: "token"
+      requires :id, type: Integer, desc: "留言id"
+      requires :content, type: String, desc: "message content"
+      requires :limit_user_accounts, type: Integer, desc: "观看人数限制"
+      requires :limit_days, type: Integer, desc: "时间限制"
+    end
+    post "update_message" do
+      u = User.find_by_token(params[:token])
+      return {status: 205, message: '没有这个用户'} if u.nil?
+      m = Message.find_by("user_id = ? and id = ?", u.id, params[:id])
+      return {status: 203, message: '没有找到'} if m.nil?
+      # info = {content: params[:content], latitude: params[:latitude], longitude: params[:longitude], limit_days: params[:limit_days].to_i, limit_user_accounts: params[:limit_user_accounts]}
+      info = {content: params[:content], limit_days: params[:limit_days].to_i, limit_user_accounts: params[:limit_user_accounts]}
+      if m.update(info)
+        return {status: 0,message: "更新成功"}
+      else
+        return {status: 204,message: m.errors.full_messages.join(",")}
+      end
+    end
+
+    desc "留言列表"
+    params do
+      requires :token, type: String, desc: "token"
+    end
+    get "show_messages" do
+      u = User.find_by_token(params[:token])
+      return {status: 205, message: '没有这个用户'} if u.nil?
+      @messages = u.messages
+      data = {}
+      data[:status] = 0
+      data[:message] = ""
+      data[:owner] = u.id
+      data[:sum] = @messages.count
+      data[:result] = @messages.map do |m|
+        {id: m.id, latitude: m.latitude, longitude: m.longitude, content: m.content, limit_days: m.limit_days}
+      end
+      return data
     end
 
     get :ping do
